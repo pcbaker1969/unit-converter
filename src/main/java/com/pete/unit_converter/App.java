b4feb4d793eec501fb2fec9db2df935990444cab
Peter Baker <peter.baker@bigbear.com>
2018-10-16 15:21:41 -0400

    My first maven commit

---
+++ src/main/java/com/pete/unit_converter/App.java
@@ -0,0 +1,13 @@
+package com.pete.unit_converter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
